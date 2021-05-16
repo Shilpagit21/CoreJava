9d78a820d63330763511d9a7e235cac522441647
shilpa <shilpa.tkumar@gmail.com>
2021-05-16 05:22:27 +0000

    extend override child

--- src/oopsconcept/METHODOVERRIDECHILD.java
+++ src/oopsconcept/METHODOVERRIDECHILD.java
@@ -3,7 +3,7 @@ package oopsconcept;
 public class METHODOVERRIDECHILD extends METODOVERRIDEPARENT {
 	public void display() {
 		
-		System.out.println("INSIDE CHILD");
+		System.out.println("INSIDE CHILD"); //extending override child from method override
 	}
 
 }
